79a7034ac6dc07dd606a57f5afaa0949b9e4ba63
napppoli <ryohayashi3571@gmail.com>
2018-07-19 16:24:37 +0000

    Finish signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
   get  '/contact', to: 'static_pages#contact'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
