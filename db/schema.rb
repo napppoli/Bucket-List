64edc62516234c6f518348b52cc17491009f6871
napppoli <ryohayashi3571@gmail.com>
2018-07-20 08:58:52 +0000

    scaffold mvc model for task. no idea how to integrate the model to my user model. abort.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180719152412) do
+ActiveRecord::Schema.define(version: 20180720080938) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.date "until"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
