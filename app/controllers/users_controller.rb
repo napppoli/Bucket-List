6d9201605cf16030b1ca0b870490e0426cbc1e43
napppoli <ryohayashi3571@gmail.com>
2018-07-20 08:06:33 +0000

    13.2.2 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
-  end
-  
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,10 +16,10 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
 end
